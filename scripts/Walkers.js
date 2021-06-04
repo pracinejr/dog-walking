bedb50a98cfaf22d55bec7462a40a170f2614634
Peter Racine Jr <peterracinejr@gmail.com>
2021-06-04 11:38:38 -0500

    added click event listener

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,4 +1,16 @@
 import { getWalkers } from "./database.js";
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  if (itemClicked.id.startsWith("walker")) {
+    const [, walkerId] = itemClicked.id.split("--");
+
+    for (const walker of walkers) {
+      if (walker.id === parseInt(walkerId)) {
+        window.alert(`${walker.name} services ${walker.city}`);
+      }
+    }
+  }
+});
 
 const walkers = getWalkers();
 
@@ -6,7 +18,7 @@ export const Walkers = () => {
   let walkerHTML = "<ul>";
 
   for (const walker of walkers) {
-    walkerHTML += `<li>${walker.name}</li>`;
+    walkerHTML += `<li id="walker--${walker.id}">${walker.name}</li>`;
   }
 
   walkerHTML += "</ul>";
